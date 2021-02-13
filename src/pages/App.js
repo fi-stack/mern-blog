f725b22ddb9b698d544c3afe4fb38463569af3d9
rafi-windows <rafi_n_o@github.com>
2021-02-13 12:43:53 +0700

    navigasi / router

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,8 @@
+import { Routes } from "../config";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world!
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
