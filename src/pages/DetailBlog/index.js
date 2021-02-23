e6ef6eecd3bafc60e6a09bd0be95dd7d0e83483e
rafi-windows <rafi_n_o@github.com>
2021-02-24 06:34:12 +0700

    styling halaman detail-blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import "./detailBlog.scss";
 
 const DetailBlog = () => {
   return (
-    <div>
-      <p>Content Detail Blog Page</p>
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBg} alt="thumb" />
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem tenetur
+        illo maiores laboriosam molestias illum ab error pariatur, repellendus
+        aut consequatur, ullam velit ut iure reprehenderit veniam? Non, dolorem
+        impedit.
+      </p>
     </div>
   );
 };
