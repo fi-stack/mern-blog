74d1ead09a8cca1df3fb1a8cdfe761558a3cacd5
rafi-windows <rafi_n_o@github.com>
2021-05-16 14:15:39 +0700

    combine reducer (multiple reducer)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  const [dataBlog, setDataBlog] = useState([]);
+  // const [dataBlog, setDataBlog] = useState([]);
+  const { dataBlogs, name } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const stateGlobal = useSelector((state) => state);
-  console.log(stateGlobal);
+  // console.log("state global: ", stateGlobal);
+  console.log("data blog global: ", dataBlogs);
   useEffect(() => {
-    Axios.get("http://localhost:4000/v1/blog/posts")
+    setTimeout(() => {
+      dispatch({ type: "UPDATE_NAME" });
+    }, 3000);
+
+    Axios.get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
       .then((result) => {
         console.log("data API", result.data);
         const responseAPI = result.data;
 
-        setDataBlog(responseAPI.data);
+        // setDataBlog(responseAPI.data);
+        dispatch({ type: "UPDATE_DATA_BLOG", payload: responseAPI.data });
       })
       .catch((err) => {
         console.log("error: ", err);
       });
-  }, []);
+  }, [dispatch]);
   const history = useHistory();
   return (
     <div className="home-page-wrapper">
@@ -31,9 +38,10 @@ const Home = () => {
           onClick={() => history.push("/create-blog")}
         />
       </div>
+      <p>{name}</p>
       <Gap height={20} />
       <div className="content-wrapper">
-        {dataBlog.map((blog) => {
+        {dataBlogs.map((blog) => {
           return (
             <BlogItem
               key={blog._id}
