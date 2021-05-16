3fa637ce11279b659c41965a5cff7d0aa0c60217
rafi-windows <rafi_n_o@github.com>
2021-05-16 16:20:16 +0700

    action creator dan async store dengan redux thunk

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -1,31 +1,7 @@
-<<<<<<< HEAD
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducer/reducer";
-=======
-const { createStore } = require("redux");
+import thunk from "redux-thunk";
 
-const initialState = {
-  dataBlogs: [],
-  name: "Rafi",
-};
-
-const reducer = (state = initialState, action) => {
-  if (action.type === "UPDATE_DATA_BLOG") {
-    return {
-      ...state,
-      dataBlogs: action.payload,
-    };
-  }
-  if (action.type === "UPDATE_NAME") {
-    return {
-      ...state,
-      name: "Nur",
-    };
-  }
-  return state;
-};
->>>>>>> 74d1ead09a8cca1df3fb1a8cdfe761558a3cacd5
-
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,41 +2,15 @@ import React, { useEffect } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
-import Axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
+import { setDataBlog } from "../../config/redux/action/homeAction";
 
 const Home = () => {
-<<<<<<< HEAD
   const { dataBlog } = useSelector((state) => state.homeReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
-=======
-  // const [dataBlog, setDataBlog] = useState([]);
-  const { dataBlogs, name } = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  // console.log("state global: ", stateGlobal);
-  console.log("data blog global: ", dataBlogs);
-  useEffect(() => {
-    setTimeout(() => {
-      dispatch({ type: "UPDATE_NAME" });
-    }, 3000);
-
->>>>>>> 74d1ead09a8cca1df3fb1a8cdfe761558a3cacd5
-    Axios.get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
-      .then((result) => {
-        const responseAPI = result.data;
-
-<<<<<<< HEAD
-=======
-        // setDataBlog(responseAPI.data);
->>>>>>> 74d1ead09a8cca1df3fb1a8cdfe761558a3cacd5
-        dispatch({ type: "UPDATE_DATA_BLOG", payload: responseAPI.data });
-      })
-      .catch((err) => {
-        console.log("error: ", err);
-      });
+    dispatch(setDataBlog());
   }, [dispatch]);
   const history = useHistory();
   return (
@@ -47,10 +21,9 @@ const Home = () => {
           onClick={() => history.push("/create-blog")}
         />
       </div>
-      <p>{name}</p>
       <Gap height={20} />
       <div className="content-wrapper">
-        {dataBlogs.map((blog) => {
+        {dataBlog.map((blog) => {
           return (
             <BlogItem
               key={blog._id}
